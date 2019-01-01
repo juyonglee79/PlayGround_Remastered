e310786fe5a8968d92655b82134415407d889564
juyonglee79 <juyonglee79@naver.com>
2019-01-01 23:50:43 +0900

    [ADD] MainActivity

--- app/src/main/java/com/dsm2018/playground_remastered/ui/main/MainActivity.kt
+++ app/src/main/java/com/dsm2018/playground_remastered/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dsm2018.playground_remastered.ui.main
 
 import android.content.Intent
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
